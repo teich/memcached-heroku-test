345ef85adca7c6cfb59bb1bbf46a1398273d4f07
Oren Teich <oren@heroku.com>
2010-06-21 19:07:54 -0700

    dumb

--- app/controllers/tester_controller.rb
+++ app/controllers/tester_controller.rb
@@ -1,6 +1,6 @@
 class TesterController < ApplicationController
   def index
     @counter = Rails.cache.read('speed-test') + 1
-    @counter = Rails.cache.write('speed-test', @counter)
+    Rails.cache.write('speed-test', @counter)
   end
 end
